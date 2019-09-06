5cefe6b250e5b2eea84d3757b6de4c9dd10c6124
Walter Hill <walterhilldev@gmail.com>
2019-09-06 02:06:09 -0400

    moved TextureFromFile into model class to fix mult-definition error

--- src/RocketEngine/asset/Model.h
+++ src/RocketEngine/asset/Model.h
@@ -33,7 +33,7 @@
 #include "../logging/RK_Log.h"
 #include <DeanLib/Trackable.h>
 
-unsigned int TextureFromFile(const char *path, const std::string &directory, bool gamma = false);
+//unsigned int TextureFromFile(const char *path, const std::string &directory, bool gamma = false);
 
 struct ModelData {
 	std::vector<Mesh> meshes;
@@ -204,52 +204,54 @@ class Model : public Trackable
 			}
 			return textures;
 		};
+
+
+		/*
+			* Loads in textures from the given file path and
+			stores the textures in an OpenGL-compatible form
+		*/
+		unsigned int TextureFromFile(const char *path, const std::string &directory, bool gamma = false)
+		{
+			std::string filename = (std::string)path;
+			filename = directory + '/' + filename;
+
+			TextureId id;
+			glGenTextures(1, &id);
+
+			int width, height, nrComponents;
+			unsigned char* data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+
+			if (data)
+			{
+				GLenum format;
+				if (nrComponents == 1)
+					format = GL_RED;
+				else if (nrComponents == 3)
+					format = GL_RGB;
+				else if (nrComponents == 4)
+					format = GL_RGBA;
+
+				glBindTexture(GL_TEXTURE_2D, id);
+				glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+				glGenerateMipmap(GL_TEXTURE_2D);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+				stbi_image_free(data);
+			}
+			else
+			{
+				RK_CORE_ERROR_ALL("ERROR::Texture::Failed to load at path: " + (std::string)path);
+				stbi_image_free(data);
+			}
+
+			return id;
+		}
+
 };
-
-/*
-	* Loads in textures from the given file path and 
-	stores the textures in an OpenGL-compatible form
-*/
-unsigned int TextureFromFile(const char *path, const std::string &directory, bool gamma)
-{
-	std::string filename = (std::string)path;
-	filename = directory + '/' + filename;
-
-	TextureId id;
-	glGenTextures(1, &id);
-
-	int width, height, nrComponents;
-	unsigned char* data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
-
-	if (data)
-	{
-		GLenum format;
-		if (nrComponents == 1)
-			format = GL_RED;
-		else if (nrComponents == 3)
-			format = GL_RGB;
-		else if (nrComponents == 4)
-			format = GL_RGBA;
-
-		glBindTexture(GL_TEXTURE_2D, id);
-		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-		stbi_image_free(data);
-	}
-	else
-	{
-		RK_CORE_ERROR_ALL("ERROR::Texture::Failed to load at path: " + (std::string)path);
-		stbi_image_free(data);
-	}
-
-	return id;
-}
 
 
 #endif // !MODEL_H
