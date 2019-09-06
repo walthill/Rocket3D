abc8b55cd141912d95f771a6768c19141747c8ed
Walter Hill <walterhilldev@gmail.com>
2019-09-06 02:05:07 -0400

    changed trackable from pragma to ifndef to prevent some build warnings

--- external/DeanLib/include/Trackable.h
+++ external/DeanLib/include/Trackable.h
@@ -1,18 +1,22 @@
-#pragma once
+#ifndef TRACKABLE_H
+#define TRACKABLE_H
 
 #include <iostream>
 
 class Trackable
 {
-public:
-	void* operator new(std::size_t size);
-	void operator delete(void *ptr);
-	void* operator new[](std::size_t size);
-	void operator delete[](void *ptr);
+	public:
+		void* operator new(std::size_t size);
+		void operator delete(void *ptr);
+		void* operator new[](std::size_t size);
+		void operator delete[](void *ptr);
 
-	//for use with placement new
-	void* operator new(std::size_t size, void* ptr){ return ptr; };
-	void operator delete(void *ptr, void*ptr2){};
-	void* operator new[](std::size_t size, void* ptr){ return ptr; };
-	void operator delete[](void *ptr, void* ptr2){};
+		//for use with placement new
+		void* operator new(std::size_t size, void* ptr) { return ptr; };
+		void operator delete(void *ptr, void*ptr2) {};
+		void* operator new[](std::size_t size, void* ptr) { return ptr; };
+		void operator delete[](void *ptr, void* ptr2) {};
 };
+
+
+#endif // !TRACKABLE_H
