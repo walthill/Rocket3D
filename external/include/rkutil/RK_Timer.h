8fb346abff3458e752d540fa0510c21c333597cf
Walter Hill <walterhilldev@gmail.com>
2019-12-06 20:22:52 -0500

    adding namespace changes to includes

--- external/include/rkutil/RK_Timer.h
+++ external/include/rkutil/RK_Timer.h
@@ -9,34 +9,37 @@
 typedef long long int64;
 typedef unsigned long long uint64;
 
-class RK_Timer {
-	public:
-		RK_Timer();
-		~RK_Timer() {};
+namespace rkutil 
+{
+	class RK_Timer 
+	{
+		public:
+			RK_Timer();
+			~RK_Timer() {};
 
-		void start();
-		void stop();
-		void pause();
+			void start();
+			void stop();
+			void pause();
 
-		double getTimeElapsedMs() const;
-	
-		double getTimeElapsedInSeconds() const;
+			double getTimeElapsedMs() const;
 
-		void sleepForMilliseconds(double ms);
+			double getTimeElapsedInSeconds() const;
 
-		void sleepUntilElapsed(double ms);
+			void sleepForMilliseconds(double ms);
 
-	private:
-		//param1: An arithmetic type, or a class emulating an arithmetic type, to be used as the type for the internal count.
-		//param2: A ratio type that represents the period in seconds. http://www.cplusplus.com/reference/chrono/duration/
-		typedef std::chrono::duration<double, std::ratio<1> > rk_second;
-		typedef std::chrono::duration<double, std::milli > rk_millisecond;
+			void sleepUntilElapsed(double ms);
 
-		typedef std::chrono::high_resolution_clock rk_clock;
-		
-		std::chrono::time_point<rk_clock> mStartTime;
-		std::chrono::time_point<rk_clock> mEndTime;
-		bool mIsPaused = false;
-};
+		private:
+			//param1: An arithmetic type, or a class emulating an arithmetic type, to be used as the type for the internal count.
+			//param2: A ratio type that represents the period in seconds. http://www.cplusplus.com/reference/chrono/duration/
+			typedef std::chrono::duration<double, std::ratio<1> > rk_second;
+			typedef std::chrono::duration<double, std::milli > rk_millisecond;
 
+			typedef std::chrono::high_resolution_clock rk_clock;
+
+			std::chrono::time_point<rk_clock> mStartTime;
+			std::chrono::time_point<rk_clock> mEndTime;
+			bool mIsPaused = false;
+	};
+}
 #endif // !RK_TIMER_H
