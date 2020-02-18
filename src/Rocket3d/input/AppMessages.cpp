6423074b79b046b6453cb4a552d22a59986085bc
Walter Hill <walterhilldev@gmail.com>
2020-02-17 21:39:37 -0500

    adding application message files

---
+++ src/Rocket3d/input/AppMessages.cpp
@@ -0,0 +1,28 @@
+#include "AppMessages.h"
+#include "../core/Application.h"
+#include <logging/RK_Log.h>
+
+AppKeyDown::AppKeyDown(const int& key)
+	:Message(BUTTON_DOWN)
+	, mKeyCode(key)
+{
+}
+
+AppKeyDown::~AppKeyDown()
+{
+}
+
+void AppKeyDown::process(float deltaTime)
+{
+	Application* app = Application::getInstance();
+
+	if (mKeyCode == ESC)
+	{
+		app->quit();
+	}
+	else if (mKeyCode == ENTER)
+	{
+		RK_LOG_C("PLAY MODE TOGGLE");
+		app->play();
+	}
+}
