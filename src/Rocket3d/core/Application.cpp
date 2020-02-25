deb4f50674c806e4394a10432145a5e53e5303bc
Walter Hill <walterhilldev@gmail.com>
2020-02-24 22:53:23 -0500

    added link for future research

--- src/Rocket3d/core/Application.cpp
+++ src/Rocket3d/core/Application.cpp
@@ -4,7 +4,8 @@
 
 /*	TODO	
  * ============================================================
- *		-- ImGui docking & viewports
+ *		-- Remove ImGui input messaging
+ *		-- Render game scene to ImGui window - https://gamedev.stackexchange.com/questions/140693/how-can-i-render-an-opengl-scene-into-an-imgui-window
  *		-- Abstract input codes
  *		-- Input polling 
  *		-- fully implement input messaging (flesh out files, etc)
